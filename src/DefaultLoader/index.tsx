36e2d4fd499ecc569f939ea25f24f2b5ff392c12
Dave-Rushabh <rushabh.dave.dev@gmail.com>
2025-03-01 12:59:22 +0530

    Initial Commit

---
+++ src/DefaultLoader/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DefaultLoader = () => {
+  return (
+    <>
+      <div className="rbac-default-loader-wrapper">
+        <span className="react-rbac-loader" />
+      </div>
+    </>
+  );
+};
+
+export default DefaultLoader;
